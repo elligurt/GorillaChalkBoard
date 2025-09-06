f8766f42a26c03a266f79df89f98c00d86780889
elliot! <benjikrolfanpageuwu@gmail.com>
2025-09-06 14:08:50 +0100

    Minor Changes

--- GorillaChalkBoard/Plugin.cs
+++ GorillaChalkBoard/Plugin.cs
@@ -1,18 +1,57 @@
-﻿using BepInEx;
+using System;
+using System.Threading.Tasks;
+using BepInEx;
+using GorillaChalkBoard.Tools;
+using UnityEngine;
 
 namespace GorillaChalkBoard
 {
     [BepInPlugin(Constants.GUID, Constants.Name, Constants.Version)]
     public class Plugin : BaseUnityPlugin
     {
-        public static Plugin Instance;
+        public static Plugin Instance { get; private set; }
 
-        void Awake()
+        private GameObject _boardPrefab;
+
+        private void Awake()
         {
-            if (Instance == null)
-                Instance = this;
+            Instance = this;
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
 
-            GorillaTagger.OnPlayerSpawned(AssetLoader.OnGameInitialized);
+        private async void OnGameInitialized(object sender, EventArgs e)
+        {
+            await SetupBoard();
+        }
+
+        private async Task SetupBoard()
+        {
+            try
+            {
+                ImageManager.CreateImageFolder();
+                ImageManager.LoadAllImages();
+
+                _boardPrefab = await AssetLoader.LoadAsset<GameObject>("GorillaChalkBoard");
+                if (_boardPrefab == null)
+                {
+                    Debug.LogError("[GorillaChalkBoard] Failed to load prefab.");
+                    return;
+                }
+
+                GameObject boardInstance = Instantiate(_boardPrefab);
+                boardInstance.SetActive(true);
+                boardInstance.transform.position = new Vector3(-69.2846f, 12.1246f, -81.8547f);
+                boardInstance.transform.rotation = Quaternion.Euler(0f, 108.5379f, 0f);
+
+                ImageManager.ApplyImagesToPhotos(boardInstance);
+
+                ErrorManager.CheckAndShowError(boardInstance);
+
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("[GorillaChalkboard] Error setting up board: " + ex);
+            }
         }
     }
 }
